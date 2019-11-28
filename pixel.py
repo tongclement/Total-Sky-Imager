2fae014a809e79fe2a3fb59daa31a58e72a40274
FalconBFR <0220182218@harrowschool.hk>
2019-11-28 20:42:46 +0800

    Empty Project

---
+++ pixel.py
@@ -0,0 +1,19 @@
+class pixel:
+    '''
+    This class should take in the pixel data
+    the calc function decides whether the pixel is part of the cloud or not and returns True or False
+            True = is a cloud
+            False= is clear
+    '''
+    def __init__(self,red,blue,green):
+        self.red = red
+        self.blue = blue
+        self.green = green
+    def calc(self):
+        '''
+        the calc method uses the values self.red, self.blue, self.green
+        a red blue balance method should be inherited
+        :return: True or False
+            True = Cloud
+            False = Clear
+        '''
