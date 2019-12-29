c7f1db600715b3844cd34dd02a5305e7c2aaaf79
FalconBFR <0220182218@harrowschool.hk>
2019-12-29 12:42:43 +0800

    Returns percentage instead and logic fixed

--- processing/processing.py
+++ processing/processing.py
@@ -5,18 +5,19 @@ def imgprocess(imgpath):
     returns:
     cloudcover (float from 0-1)
     '''
-    mask=[200,200,200]
+    mask=[250,250,250]
     masked=0 #pixels
     import cv2
     img=cv2.imread(imgpath)
-    #from matplotlib import pyplot as plt
-    #import numpy as np
-    #print(img[311,195])
-    #print(img.shape[2])
+    # from matplotlib import pyplot as plt
+    # import numpy as np
     for x in range(img.shape[0]):
         for y in range(img.shape[1]):
-            for z in range(3):
-                if (img[x,y,z]>=mask[z]):  masked+=1
-    return masked
+            currentloop=0
+            for z in range(img.shape[2]):
+                if (img[x,y,z]>=mask[z]):currentloop+=1
+                else: currentloop=0; break
+            masked+=(currentloop/img.shape[2])
+    return masked/(img.shape[0]*img.shape[1])
 
 print(imgprocess('asc_hksm_h12m50-cropped.jpg'))
