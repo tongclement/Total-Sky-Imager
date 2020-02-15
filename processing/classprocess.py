aca6c0e81739ad6f7ed4a1681b53184aa432f969
FalconBFR <0220182218@harrowschool.hk>
2020-02-15 23:01:40 +0800

    With a working annotation system but code base is messed up

--- processing/classprocess.py
+++ processing/classprocess.py
@@ -16,6 +16,17 @@ class Process:
         :param color: [r,g,b] (r,g,b stored in a list)
         :return: None
         '''
+        #from .annotate import annotate
+        import annotate
+        #import moduletest
+        image = cv2.imread(self.imgpath, 1)
+        color = [92,209,255]
+        for x in range(0, 100):
+            for y in range(0, 100):
+                #testing.annotate([x, y], [92, 209, 255])
+                 image = annotate.annotate(image, color[0], color[1], color[2], x, y)
+        return image
+
 
 
 
@@ -23,8 +34,18 @@ class Process:
 testing=Process('test_offical.jpg')
 testing2=Process('test_white2.jpg')
 testing3=Process('test_black.png')
-print(float(testing.calc_ccover())*100)
-print(testing2.calc_ccover())
-print(testing3.calc_ccover())
-
+#print(float(testing.calc_ccover())*100)
+#print(testing2.calc_ccover())
+#print(testing3.calc_ccover())
+# for x in range(0,100):
+#     for y in range(0,100):
+#         testing.annotate([x,y],[92,209,255])
+import cv2
+newimg = testing.annotate([0,0],[92,209,255])
+cv2.imshow('image', newimg)
+import time
+time.sleep(60.0)
+print('bequick')
+#cv2.waitKey(0)
+cv2.destroyAllWindows()
 
