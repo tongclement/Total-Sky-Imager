312e6125e562a49169932946918f864db0f79921
FalconBFR <0220182218@harrowschool.hk>
2020-02-26 21:23:20 +0800

    Fixed bug where it always annotate whether it is a cloud or not

--- processing/processing.py
+++ processing/processing.py
@@ -5,7 +5,7 @@ def imgprocess(imgpath):
     returns:
     cloudcover (float from 0-1)
     '''
-    mask=[180,180,180]
+    mask=[255,255,255]
     mask1=[]
     masked=0 #pixels
     import cv2
@@ -19,14 +19,15 @@ def imgprocess(imgpath):
             currentloop=0
             for z in range(img.shape[2]):
                 if (img[y,x,z]>=mask[z]):
-                    #if y>391:
-                        #print('yes failed ------------------------------------------------')
                     currentloop+=1
+                    print('2222222-----------')
                 else:
-                    currentloop=0; break
+                    currentloop=0;break
+                    print('this is the case')
             #all the R,G,B values meet the requirements
-            mask1.append([x, y]) #x and y values appended to the list mask1 as a list [x,y] for annotation
-            masked+=(currentloop/img.shape[2]) #+=1 to number of pixels masked
+            if currentloop!=0:
+                masked+=(currentloop/img.shape[2]) #+=1 to number of pixels masked
+                mask1.append([x, y])  # x and y values appended to the list mask1 as a list [x,y] for annotation
     return [(masked/(img.shape[0]*img.shape[1])),mask1]
 
 
