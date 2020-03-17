e13fc367bd51fd2389c031eb63c4b7acbae451a0
FalconBFR <0220182218@harrowschool.hk>
2020-03-17 16:42:17 +0800

    Improved annotation - allows 2 types of objects to be annotated (light and heavy clouds). More to be added later

--- processing/classprocess.py
+++ processing/classprocess.py
@@ -2,17 +2,21 @@ class Process:
     def __init__(self,imgpath): #image path, cloud cover
         self.imgpath=imgpath #imagepath
         self.ccover=int(0) #cloudcover
-        self.cloudpixels = []
+        self.heavycloudpixels = []
+        self.lightcloudpixels = []
     def __str__(self):
         return self.ccover
     def get_imgpath(self):
         return self.imgpath
     def calc_ccover(self):
         import processing
-        self.cloudpixels = []
-        processed = processing.imgprocess(self.imgpath) # list [ cloud cover, [[x1,y1],[x2,y2],...]
+        self.heavycloudpixels = []
+        heavymask = [180,120,120] #bgr
+        lightmask = [170,90,50] #bgr
+        processed = processing.imgprocess(self.imgpath,heavymask,lightmask) # list [ cloud cover, [[x1,y1],[x2,y2],...]
         self.ccover= processed[0]
-        self.cloudpixels = processed[1] #reading the returned list from 1 to end
+        self.heavycloudpixels = processed[1] #reading the returned list from 1 to end
+        self.lightcloudpixels = processed[2]
         return self.ccover
     def annotate(self):
         '''
@@ -23,40 +27,37 @@ class Process:
         #from .annotate import annotate
 
         self.calc_ccover() #making sure this function is runned beforehand since data from this is needed
-        print(self.calc_ccover())
-        #print(self.cloudpixels)
-        print('------')
-        #print(self.cloudpixels)
         import annotate
-        #import moduletest
         image = cv2.imread(self.imgpath, 1)
-        color = [155,99,255]
+        annotatecolorheavy = [155,99,255]
+        annotatecolorlight = [200,200,200]
 
-        print(len(self.cloudpixels))
+        print(len(self.heavycloudpixels))
         import time
         print('delaying for you to look at the length of self.cloudpixel')
         time.sleep(1)
-        imager = image
-        for character in self.cloudpixels:
-            print('working')
-            x = character[0]
-            print(x)
-            y = character[1]
-            imager = annotate.annotate(imager, color[0], color[1], color[2], x, y)
-            #print(image == imager)
-        #for x in range(0, 100):
-         #   for y in range(0, 100):
-                #testing.annotate([x, y], [92, 209, 255])
-        cv2.imshow('image',imager)
+
+        def actualannoate(pixels,imager,color):
+            for character in pixels:
+                print('working')
+                x = character[0]
+                print(x)
+                y = character[1]
+                imager = annotate.annotate(imager, color[0], color[1], color[2], x, y)
+            return imager
+
+        mimage = actualannoate(self.lightcloudpixels,image,annotatecolorlight) #original , annotating light first since it should cover more areas and avoid overiding the color of the heavy cloud part
+        mimage = actualannoate(self.heavycloudpixels,mimage,annotatecolorheavy) #use mutated image = mimage
+        cv2.imshow('image',mimage)
         print('ended')
-        return imager
+        return mimage
 
 
 
 
 #testing=Process('asc_hksm_h12m50-cropped.jpg')
 #testing=Process('test_offical.jpg')
-testing = Process('test_black.png')
+testing = Process('TaiMoShanMixedLand.jpg')
 #testing2=Process('test_white2.jpg')
 #testing3=Process('test_black.png')
 #print(float(testing.calc_ccover())*100)
--- processing/processing.py
+++ processing/processing.py
@@ -1,34 +1,43 @@
-def imgprocess(imgpath):
+def imgprocess(imgpath, heavymask,lightmask):
     '''
     input:
     imgpath = full path to image
     returns:
     cloudcover (float from 0-1)
     '''
-    mask=[255,255,255]
-    mask1=[]
-    masked=0 #pixels
+    #mask=[180,120,120] #bgr
     import cv2
+
+    mask1=[]
+    mask2=[]
+    masked=0 #pixels
+
     img=cv2.imread(imgpath)
-    # from matplotlib import pyplot as plt
-    # import numpy as np
+
     print(img.shape[0])
     print(img.shape[1])
     for x in range(img.shape[1]):
         for y in range(img.shape[0]):
             currentloop=0
+            currentcloudtype=('heavy')
             for z in range(img.shape[2]):
-                if (img[y,x,z]>=mask[z]):
+                if (img[y,x,z]>=heavymask[z]):
                     currentloop+=1
                     print('2222222-----------')
                 else:
-                    currentloop=0;break
+                    if (img[y,x,z]>=lightmask[z]):
+                        currentcloudtype=('light')
+                    else:
+                        currentloop=0;break
                     print('this is the case')
             #all the R,G,B values meet the requirements
             if currentloop!=0:
                 masked+=(currentloop/img.shape[2]) #+=1 to number of pixels masked
-                mask1.append([x, y])  # x and y values appended to the list mask1 as a list [x,y] for annotation
-    return [(masked/(img.shape[0]*img.shape[1])),mask1]
+                if currentcloudtype==('heavy'):
+                    mask1.append([x, y])  # x and y values appended to the list mask1 as a list [x,y] for annotation
+                elif currentcloudtype==('light'):
+                    mask2.append([x,y])
+    return [(masked/(img.shape[0]*img.shape[1])),mask1,mask2]
 
 
 #for testing ------- ---------------------------
